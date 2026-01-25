fc8f1c68e3153df6a540adbf53da18a586983681
Matthew DeVerna <mdeverna@iu.edu>
2026-01-25 12:53:51 -0800

    remove pypi test flag

--- publish.sh
+++ publish.sh
@@ -7,9 +7,5 @@ rm -rf dist/ build/ src/*.egg-info
 # Build
 uv build
 
-# Upload (use --publish-url for TestPyPI dry runs)
-if [[ "${1:-}" == "--test" ]]; then
-    uv publish --publish-url https://test.pypi.org/legacy/
-else
-    uv publish
-fi
+# Upload to PyPI
+uv publish
